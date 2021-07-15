b6740da69ae57ebc69cb92f45473013991f3a2bf
xiaojiang <1215683942@qq.com>
2021-07-15 23:53:24 +0800

    1

---
+++ Test/__init__.py
@@ -0,0 +1,12 @@
+# listBoss = [1, 2, 3, 4]
+#
+# listBoss.insert(int(len(listBoss) / 2), 5)
+#
+# print(len(listBoss))
+# listBoss.sort(reverse=True)
+# print(listBoss)
+
+numbers = list(range(1, 1_000_000))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
