18130fecdd3f865718b4d32875d70dbc6e7e52a8
xiaojiang <1215683942@qq.com>
2021-07-16 20:35:19 +0800

    1

--- Test/__init__.py
+++ Test/__init__.py
@@ -6,7 +6,27 @@
 # listBoss.sort(reverse=True)
 # print(listBoss)
 
-numbers = list(range(1, 1_000_000))
-print(max(numbers))
-print(min(numbers))
-print(sum(numbers))
+# numbers = list(range(1, 1_000_000))
+# print(max(numbers))
+# print(min(numbers))
+# print(sum(numbers))
+
+# get list's mid 3 num or 2 num
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [1, 2, 3, 4, 5]
+lists = (list1, list2)
+
+for list in lists:
+    if len(list) % 2 == 0:
+        print(list[int(len(list) / 2) - 1:
+                   int(len(list) / 2) + 1])
+    else:
+        print(list[int(len(list) / 2) - 1:
+                   int(len(list) / 2) + 2])
+
+print(1 not in list1)
+# # try the pinyin plug
+# 列表 = (1, 2)
+# # 'tuple' object does not support item assignment
+# # 列表[0] = 1
+# print(列表)
---
+++ Test/alienTest.py
@@ -0,0 +1,7 @@
+favorite_languages = {
+    'jen': 'c',
+    'jiang': 'java'
+}
+
+language = favorite_languages['jiang']
+print(f"jiang's favorite language is {language}" + language)
